91c09585790c34e4e0e50c02d05fa7ae436476d8
邱達儒 <1234567si2k@gmail.com>
2023-10-24 21:40:53 +0800

    1

---
+++ blockly/blocks.js
@@ -0,0 +1,11 @@
+Blockly.Blocks['nft'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("NFT");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
