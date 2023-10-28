de41935672719ef8b3510215ef617c5010d265d6
邱達儒 <1234567si2k@gmail.com>
2023-10-28 12:07:58 +0800

    1

--- blockly/blockly/javascript.js
+++ blockly/blockly/javascript.js
@@ -1,4 +1,4 @@
-jjavascript.javascriptGenerator.forBlock['twoblockly'] = function(block, generator) {
+javascript.javascriptGenerator.forBlock['twoblockly'] = function(block, generator) {
   // TODO: Assemble javascript into code variable.
   var code = 'console.log("hello");\n';
   return code;
